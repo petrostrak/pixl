a447d15d751501a8926cfe8989b39696511f3c52
petrostrak <pit.trak@gmail.com>
2022-03-28 16:14:35 +0300

    Add apptype file and structs

---
+++ apptype/apptype.go
@@ -0,0 +1,23 @@
+package apptype
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+)
+
+type BrushType = int
+
+type PxCanvasConfig struct {
+	DrawingArea    fyne.Size
+	CanvasOffset   fyne.Position
+	PxRows, PxCols int
+	PxSize         int
+}
+
+type State struct {
+	BrushColor     color.Color
+	BrushType      int
+	SwatchSelected int
+	FilePath       string
+}
