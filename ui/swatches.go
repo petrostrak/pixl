8ccff0c39a40af2e0430aa0dbf3692fb383ad5b5
petrostrak <pit.trak@gmail.com>
2022-03-29 10:40:38 +0300

    Add BuildSwatches func

--- ui/swatches.go
+++ ui/swatches.go
@@ -1,1 +1,35 @@
 package ui
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"github.com/petrostrak/pixl/swatch"
+)
+
+func BuildSwatches(a *AppInit) *fyne.Container {
+	canvasSwatches := make([]fyne.CanvasObject, 0, 64)
+	for i := 0; i < cap(a.Swatches); i++ {
+		initialColor := color.NRGBA{255, 255, 255, 255}
+		s := swatch.NewSwatch(a.State, initialColor, i, func(s *swatch.Swatch) {
+			for j := 0; j < len(a.Swatches); j++ {
+				a.Swatches[j].Selected = false
+				canvasSwatches[j].Refresh()
+			}
+			a.State.SwatchSelected = s.SwatchIndex
+			a.State.BrushColor = s.Color
+		})
+
+		if i == 0 {
+			s.Selected = true
+			a.State.SwatchSelected = 0
+			s.Refresh()
+		}
+
+		a.Swatches = append(a.Swatches, s)
+		canvasSwatches = append(canvasSwatches, s)
+	}
+
+	return container.NewGridWrap(fyne.NewSize(20, 20), canvasSwatches...)
+}
