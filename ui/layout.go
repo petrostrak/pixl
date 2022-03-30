9f3cddec658ba12f961431f8e54fe4f0a071ee88
petrostrak <pit.trak@gmail.com>
2022-03-30 21:08:33 +0300

    Update Setup func

--- ui/layout.go
+++ ui/layout.go
@@ -1,7 +1,12 @@
 package ui
 
+import "fyne.io/fyne/v2/container"
+
 func Setup(app *AppInit) {
 	swatchesContainer := BuildSwatches(app)
+	colorPicker := SetupColorPicker(app)
 
-	app.PixlWindow.SetContent(swatchesContainer)
+	appLayout := container.NewBorder(nil, swatchesContainer, nil, colorPicker)
+
+	app.PixlWindow.SetContent(appLayout)
 }
