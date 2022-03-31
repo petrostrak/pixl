1569ad78dfd5fc9c86360ecd4b906fd9f2666e1d
petrostrak <pit.trak@gmail.com>
2022-03-31 10:49:52 +0300

    Update main and Setup funcs

--- pixl/pixl.go
+++ pixl/pixl.go
@@ -3,8 +3,10 @@ package main
 import (
 	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"github.com/petrostrak/pixl/apptype"
+	"github.com/petrostrak/pixl/pxcanvas"
 	"github.com/petrostrak/pixl/swatch"
 	"github.com/petrostrak/pixl/ui"
 )
@@ -18,7 +20,18 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		DrawingArea:  fyne.NewSize(600, 600),
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRows:       10,
+		PxCols:       10,
+		PxSize:       30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
 		PixlWindow: pixlWindow,
 		State:      &state,
 		Swatches:   make([]*swatch.Swatch, 0, 64),
--- ui/types.go
+++ ui/types.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"github.com/petrostrak/pixl/apptype"
+	"github.com/petrostrak/pixl/pxcanvas"
 	"github.com/petrostrak/pixl/swatch"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
