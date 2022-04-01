5ad21813c39ddd63524eae2dbd9b549b94053d52
petrostrak <pit.trak@gmail.com>
2022-04-01 18:51:24 +0300

    Add Pan method

--- pxcanvas/ops.go
+++ pxcanvas/ops.go
@@ -1,5 +1,16 @@
 package pxcanvas
 
+import "fyne.io/fyne/v2"
+
+func (pxCanvas *PxCanvas) Pan(previousCoord, currentCoord fyne.PointEvent) {
+	xDiff := currentCoord.Position.X - previousCoord.Position.X
+	yDiff := currentCoord.Position.Y - previousCoord.Position.Y
+
+	pxCanvas.CanvasOffset.X += xDiff
+	pxCanvas.CanvasOffset.Y += yDiff
+	pxCanvas.Refresh()
+}
+
 func (pxCanvas *PxCanvas) scale(direction int) {
 	switch {
 	case direction > 0:
