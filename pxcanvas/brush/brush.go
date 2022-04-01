2b80687a0105825cd53b5fddd2b6468069f5eeed
petrostrak <pit.trak@gmail.com>
2022-04-01 22:32:27 +0300

    Add TryBrush func

--- pxcanvas/brush/brush.go
+++ pxcanvas/brush/brush.go
@@ -9,6 +9,15 @@ const (
 	Pixel = iota
 )
 
+func TryBrush(appState *apptype.State, canvas apptype.Brushable, ev *desktop.MouseEvent) bool {
+	switch {
+	case appState.BrushType == Pixel:
+		return TryPaintPixel(appState, canvas, ev)
+	default:
+		return false
+	}
+}
+
 func TryPaintPixel(appState *apptype.State, canvas apptype.Brushable, ev *desktop.MouseEvent) bool {
 	x, y := canvas.MouseToCanvasXY(ev)
 	if x != nil && y != nil && ev.Button == desktop.MouseButtonPrimary {
