642545729e03e3313eaae3ab84422649f9b0667e
petrostrak <pit.trak@gmail.com>
2022-04-01 18:58:40 +0300

    Add MouseMouve

--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -1,8 +1,21 @@
 package pxcanvas
 
-import "fyne.io/fyne/v2"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
+)
 
 func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
 	pxCanvas.scale(int(ev.Scrolled.DY))
 	pxCanvas.Refresh()
 }
+
+func (pxCanvas *PxCanvas) MouseMouve(ev *desktop.MouseEvent) {
+	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
+	pxCanvas.Refresh()
+	pxCanvas.mouseState.previousCoord = &ev.PointEvent
+}
+
+func (pxCanvas *PxCanvas) MouseIn(ev *desktop.MouseEvent) {}
+
+func (pxCanvas *PxCanvas) MouseOut() {}
