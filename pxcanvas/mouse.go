03b0701ad7e1e46e1a45e5f4a6bfb31ce39074a9
petrostrak <pit.trak@gmail.com>
2022-04-02 17:18:38 +0300

    Update MouseMouve to use cursor

--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -12,6 +12,13 @@ func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
 }
 
 func (pxCanvas *PxCanvas) MouseMouve(ev *desktop.MouseEvent) {
+	if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
+		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+		cursor := brush.Cursor(pxCanvas.PxCanvasConfig, pxCanvas.appState.BrushType, ev, *x, *y)
+		pxCanvas.renderer.SetCursor(cursor)
+	} else {
+		pxCanvas.renderer.SetCursor(make([]fyne.CanvasObject, 0))
+	}
 	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
 	pxCanvas.Refresh()
 	pxCanvas.mouseState.previousCoord = &ev.PointEvent
